c9156590f4ca1b83099b462b82e7f647c9a861ed
CantSayIHave <doug.m.mcld@gmail.com>
2018-06-14 22:45:23 -0600

    documentation and implementing `File` class for easy and centralized file object info and storage

--- server/util.py
+++ server/util.py
@@ -1,6 +1,28 @@
+import io
+import os
+import time
+
 
 class TranslatedPath:
     """Takes a raw path and breaks it into parts
+
+    Supported Operations:
+
+    +-----------+--------------------------------------+
+    | Operation |             Description              |
+    +===========+======================================+
+    | x == y    | Checks if two paths are equal.       |
+    +-----------+--------------------------------------+
+    | x != y    | Checks if two paths are not equal.   |
+    +-----------+--------------------------------------+
+    | x += y    | Appends to a translated path         |
+    +-----------+--------------------------------------+
+    | str(x)    | Returns the translated path          |
+    +-----------+--------------------------------------+
+    | len(x)    | Returns the number of path scopes    |
+    +-----------+--------------------------------------+
+    | x[y]      | Returns a scope of the path          |
+    +-----------+--------------------------------------+
 
     Attributes
     ----------
@@ -16,8 +38,8 @@ class TranslatedPath:
         the top directory in the path
     resource : str
         the file or directory the path locates
-    file
-        a byte-like object loaded from file path
+    file : :class:`File`
+        a `File` container for a file-like object defined by the path
 
     """
     def __init__(self, raw: str):
@@ -94,6 +116,7 @@ class TranslatedPath:
         return self.parts[-1]
 
     def get(self):
+        """Returns a file-like object if it exists"""
         try:
             return open(self.path, 'rb')
         except:
@@ -115,7 +138,101 @@ class TranslatedPath:
 
     @property
     def file(self):
+        """Returns :class:`File` container for path-defined file"""
         if not self._file:
-            _file = self.get()
+            f_obj = self.get()
+            self._file = File(f_obj)
         return self._file
 
+    @file.setter
+    def file(self, thing):
+        if not isinstance(thing, File):
+            self._file = File(thing)
+        else:
+            self._file = thing
+
+    def replace(self, old: str, new: str, count: int = -1):
+        self.update(self.path.replace(old, new, count))
+        return self.path
+
+
+# Standardizes determination of file size
+# Upon testing, `len(f.read())` is by far the fastest method
+def get_file_size(buffer) -> int:
+    """Find the size of a file object
+
+    Parameters
+    ----------
+    buffer
+        a file object
+
+    Returns
+    -------
+    int
+        a file size in bytes
+
+    """
+    if isinstance(buffer, (io.BytesIO, io.StringIO)):
+        buffer.seek(0)
+        size = len(buffer.read())
+        buffer.seek(0)
+        return size
+    else:
+        return os.path.getsize(buffer.fileno())
+
+
+def get_file_time(buffer) -> int:
+    """Find the modify time of a file object
+
+    Parameters
+    ----------
+    buffer
+        a file object
+
+    Returns
+    -------
+    int
+        a file 'last modified' time in seconds
+
+    """
+    if isinstance(buffer, (io.BytesIO, io.StringIO)):
+        return int(time.time())
+    else:
+        return os.path.getmtime(buffer.fileno())
+
+
+class File:
+    def __init__(self, buffer, size=0, _time=0, name='', type=''):
+        self.buffer = buffer
+        self.size = size
+        self.time = _time
+        self.name = name
+        self.type = type
+        # and more
+
+        if not name:
+            try:
+                self.name = buffer.name
+            except:
+                self.name = 'file'  # creative!
+
+        if not size:
+            self.size = get_file_size(buffer)
+
+        if not _time:
+            self.time = int(time.time())
+
+        if not type and '.' in self.name:
+            self.type = self.name.rsplit('.', 1)[-1]
+
+    def __str__(self):
+        return 'File(buffer={}, size={}, time={}, name={})'.format(self.buffer, self.size, self.time, self.name)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def get(self):
+        return self.buffer
+
+    def __bytes__(self):
+        return self.buffer
