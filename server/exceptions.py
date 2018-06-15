a5667af2ebcadd4660d197af58d99ae821a98836
CantSayIHave <doug.m.mcld@gmail.com>
2018-06-14 22:44:25 -0600

    minor change to allow `explain`

--- server/exceptions.py
+++ server/exceptions.py
@@ -2,12 +2,17 @@ from http import HTTPStatus
 
 
 class HTTPException(Exception):
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args)
-        status = args[0]
+        if args:
+            status = args[0]
+        else:
+            status = kwargs.get('status', HTTPStatus.NOT_FOUND)
+
         self.status = status
         self.code = status.value
         self.message = status.phrase
+        self.explain = kwargs.get('explain', None)
 
     def __str__(self):
         return '{} - {}'.format(self.code, self.message)
