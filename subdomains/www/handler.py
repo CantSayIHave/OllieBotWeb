1a4566ac3175a05f0daeedd359d2755e900c2651
CantSayIHave <doug.m.mcld@gmail.com>
2018-06-14 22:46:35 -0600

    changes to support `File` object

--- subdomains/www/handler.py
+++ subdomains/www/handler.py
@@ -13,35 +13,33 @@ class WWWHandler(SubdomainHandler):
         self.check_errors()
 
         self.send_response(HTTPStatus.OK)
-        self.send_headers()
-        self.set_file(self.translated.file)
+        self.send_headers(self.t_path.file)
+        self.set_file(self.t_path.file)
 
     def map_path(self):
-        """Maps the requested path and adds '.html' extension if necessary"""
+        """Maps the requested path and adds '.html' extension if necessary (default guess)"""
 
-        registry.map(self.translated)
+        registry.map(self.t_path)
 
-        if not self.translated.resource.endswith(('.html', '.css', '.jpg', '.png', '.ico')):  # default to html
-            self.translated += '.html'
+        if not self.t_path.resource.endswith(('.html', '.css', '.jpg', '.png', '.ico')):  # default to html
+            self.t_path += '.html'
 
     def check_errors(self):
         """Checks for errors with security or file existence"""
 
-        if not self.translated.topdir in registry.allowed_directories:
+        if not self.t_path.topdir in registry.allowed_directories:
             raise HTTPException(HTTPStatus.FORBIDDEN)
 
-        if not self.translated.exists():
+        if not self.t_path.exists():
             raise HTTPException(HTTPStatus.NOT_FOUND)
 
-    def send_headers(self):
+    def send_headers(self, file: File):
         """Adds appropriate headers based on file"""
 
-        content_type = self.extensions_map['.' + self.translated.filetype]
-        file_stat = os.fstat(self.translated.file.fileno())
-        file_size = file_stat.st_size
-        file_time = file_stat.st_mtime
+        content_type = self.extensions_map['.' + file.type]
 
         self.send_header('Content-Type', content_type)
-        self.send_header('Content-Length', file_size)
-        self.send_header("Last-Modified", self.date_time_string(file_time))
+        self.send_header('Content-Length', file.size)
+        self.send_header('Last-Modified', self.date_time_string(file.time))
+        self.send_header('Content-Disposition', 'inline; filename="{}"'.format(file.name))
         self.end_headers()
